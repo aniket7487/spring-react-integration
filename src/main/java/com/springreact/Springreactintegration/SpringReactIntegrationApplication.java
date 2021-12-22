07b47a8bca1e483905bff9c5d3426fd5b9ba034f
aniket7487 <aniket.mane84@gmail.com>
2021-12-22 17:39:05 +0530

    Initial Project

---
+++ src/main/java/com/springreact/Springreactintegration/SpringReactIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.springreact.Springreactintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springreact/Springreactintegration/SpringReactIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springreact.Springreactintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
