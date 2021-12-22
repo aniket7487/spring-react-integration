251bae2eb0484d21afe53b7be573d6e539196e70
Aniket Mane <aniket.mane84@gmail.com>
2021-12-23 00:48:40 +0530

    accept input from react

--- src/main/java/com/springreact/Springreactintegration/controller/HomeController.java
+++ src/main/java/com/springreact/Springreactintegration/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.springreact.Springreactintegration.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -11,5 +12,10 @@ public class HomeController {
 	public String helloWorld() {
 		System.out.println("Inside HelloWorld() method");
 		return "Hello World";
+	}
+	@GetMapping("/home/{name}")
+	public String helloName(@PathVariable String name) {
+		System.out.println("Inside HelloWorld() method "+ name );
+		return "Hello " + name;
 	}
 }
