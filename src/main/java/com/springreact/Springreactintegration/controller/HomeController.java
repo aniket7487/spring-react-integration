94976667fbcb0c4442fe4db258d21e3243cfd2bb
Aniket Mane <aniket.mane84@gmail.com>
2021-12-23 00:30:28 +0530

    Send Response Hello World To React Application

--- src/main/java/com/springreact/Springreactintegration/controller/HomeController.java
+++ src/main/java/com/springreact/Springreactintegration/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.springreact.Springreactintegration.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class HomeController {
-	
+	@GetMapping("/home")
 	public String helloWorld() {
+		System.out.println("Inside HelloWorld() method");
 		return "Hello World";
 	}
 }
