73aaf43959a383cf7dad0c576bf8eb611a98af70
ANIKET7487 <aniket.mane84@gmail.com>
2024-09-29 18:45:03 +0530

    Changes in Master branch
    (cherry picked from commit f04f760bf6ecdeea8ec2ab999978634bc3c5614f)

--- src/main/java/com/springreact/Springreactintegration/SpringReactIntegrationApplication.java
+++ src/main/java/com/springreact/Springreactintegration/SpringReactIntegrationApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringReactIntegrationApplication {
-
+//Change in master branch
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactIntegrationApplication.class, args);
 	}
